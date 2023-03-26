064dc2e6fb72c5fabc829b2edb946243d3217388
Maksim Posysoev <37895103+mposysoev@users.noreply.github.com>
2023-03-26 15:10:33 +0200

    (fix): refactored by chatgpt

--- function.py
+++ function.py
@@ -3,34 +3,32 @@ import sys
 
 def replace_function(big_string, replace_what, replace_with):
     # Replace all instances of "replace_what(" with "replace_with["
-    index = 0
-    length = len(replace_what) + 1
-    while True:
-        index = big_string.find(f"{replace_what}(", index)
+    start_index = 0
+    while start_index < len(big_string):
+        index = big_string.find(f"{replace_what}(", start_index)
         if index == -1:
             break
-        big_string = (
-            big_string[:index] + f"{replace_with}[" + big_string[index + length :]
-        )
+        big_string = f"{big_string[:index]}{replace_with}[{big_string[index+len(replace_what)+1:]}"
+        start_index = index + len(replace_with) + 2
 
     # Add a closing bracket for each "replace_with[" expression
-    index = 0
-    while True:
-        index = big_string.find(f"{replace_with}[", index)
+    start_index = 0
+    while start_index < len(big_string):
+        index = big_string.find(f"{replace_with}[", start_index)
         if index == -1:
             break
         bracket_count = 1
-        j = index + length
+        j = index + len(replace_with) + 1
         while bracket_count > 0 and j < len(big_string):
             if big_string[j] == "(":
                 bracket_count += 1
             elif big_string[j] == ")":
                 bracket_count -= 1
                 if bracket_count == 0 and big_string[j] == ")":
-                    big_string = big_string[:j] + "]" + big_string[j + 1 :]
+                    big_string = f"{big_string[:j]}]{big_string[j+1:]}"
             j += 1
 
-        index = j
+        start_index = index + len(replace_with) + 1
 
     return big_string
 
